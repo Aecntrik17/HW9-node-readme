fa3ddb0c8a3cd7a45ffde24ba1b2d119b8f49c1b
Aaron Sims <aaron@adsimsllc.com>
2020-07-18 15:31:44 -0400

    creating user question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -6,7 +6,55 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your README?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please descriibe your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "please outline the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "please outline the usage information",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "please outline any contributing guidelines",
+    name: "contributing",
+  },
+  {
+    type: "list",
+    message: "Please choose a license?",
+    name: "license",
+    choices: ["Apache 2.0", "Boost 1.0", "Eclipse 1.0"],
+  },
+  {
+    type: "checkbox",
+    message: "Which tests did you run?",
+    name: "tests",
+    choices: ["test A", "test B", "test C"],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
